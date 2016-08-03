a32881c16c556c7db8d94d05bb9de3048ec5880b
binkybear <binkybear@nethunter.com>
2016-08-03 14:22:28 -0700

    Revert "[PATCH] Implement kexec-hardboot"
    This reverts commit 16d0b8418b8a74d65face0165dede42fd6517c7f.

--- arch/arm/mach-msm/oppo/board-8974-oppo.c
+++ arch/arm/mach-msm/oppo/board-8974-oppo.c
@@ -14,6 +14,7 @@
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
 #include <linux/io.h>
+#include <linux/ion.h>
 #include <linux/irq.h>
 #include <linux/irqdomain.h>
 #include <linux/of.h>
@@ -24,8 +25,6 @@
 #include <linux/regulator/krait-regulator.h>
 #include <linux/msm_tsens.h>
 #include <linux/msm_thermal.h>
-#include <linux/gpio.h>
-#include <linux/persistent_ram.h>
 #include <asm/mach/map.h>
 #include <asm/hardware/gic.h>
 #include <asm/mach/map.h>
@@ -34,7 +33,7 @@
 #include <mach/gpiomux.h>
 #include <mach/msm_iomap.h>
 #ifdef CONFIG_ION_MSM
-#include <mach/ion.h>
+#include <linux/msm_ion.h>
 #endif
 #include <mach/msm_memtypes.h>
 #include <mach/msm_smd.h>
@@ -50,68 +49,32 @@
 #include "pm.h"
 #include "modem_notifier.h"
 #include "platsmp.h"
+
+#include <linux/persistent_ram.h>
+#include <linux/gpio.h>
+
 #include <linux/pcb_version.h>
 
-#ifdef CONFIG_KEXEC_HARDBOOT
-#include <asm/setup.h>
-#include <asm/memory.h>
-#include <linux/memblock.h>
-#define OPPO_PERSISTENT_RAM_SIZE	(SZ_1M)
-#endif
+static struct platform_device *ram_console_dev;
 
-
-static struct memtype_reserve msm8974_reserve_table[] __initdata = {
-	[MEMTYPE_SMI] = {
-	},
-	[MEMTYPE_EBI0] = {
-		.flags	=	MEMTYPE_FLAGS_1M_ALIGN,
-	},
-	[MEMTYPE_EBI1] = {
-		.flags	=	MEMTYPE_FLAGS_1M_ALIGN,
+static struct persistent_ram_descriptor msm_prd[] __initdata = {
+	{
+		.name = "ram_console",
+		.size = SZ_1M,
 	},
 };
 
-static int msm8974_paddr_to_memtype(phys_addr_t paddr)
-{
-	return MEMTYPE_EBI1;
-}
-
-static struct reserve_info msm8974_reserve_info __initdata = {
-	.memtype_reserve_table = msm8974_reserve_table,
-	.paddr_to_memtype = msm8974_paddr_to_memtype,
+static struct persistent_ram msm_pr __initdata = {
+	.descs = msm_prd,
+	.num_descs = ARRAY_SIZE(msm_prd),
+	.start = PLAT_PHYS_OFFSET + SZ_1G + SZ_256M,
+	.size = SZ_1M,
 };
 
 void __init msm_8974_reserve(void)
 {
-	#ifdef CONFIG_KEXEC_HARDBOOT
-	// Reserve space for hardboot page - just after ram_console,
-	// at the start of second memory bank
-	int ret;
-	phys_addr_t start;
-	struct membank* bank;
-	
-	if (meminfo.nr_banks < 2) {
-		pr_err("%s: not enough membank\n", __func__);
-		return;
-	}
-	
-	bank = &meminfo.bank[1];
-	start = bank->start + SZ_1M + OPPO_PERSISTENT_RAM_SIZE;
-	ret = memblock_remove(start, SZ_1M);
-	if(!ret)
-		pr_info("Hardboot page reserved at 0x%X\n", start);
-	else
-		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
-	#endif
-	reserve_info = &msm8974_reserve_info;
-	of_scan_flat_dt(dt_scan_for_memory_reserve, msm8974_reserve_table);
-	msm_reserve();
-}
-
-static void __init msm8974_early_memory(void)
-{
-	reserve_info = &msm8974_reserve_info;
-	of_scan_flat_dt(dt_scan_for_memory_hole, msm8974_reserve_table);
+	persistent_ram_early_init(&msm_pr);
+	of_scan_flat_dt(dt_scan_for_memory_reserve, NULL);
 }
 
 /*
@@ -122,7 +85,6 @@ static void __init msm8974_early_memory(void)
  */
 void __init msm8974_add_drivers(void)
 {
-	msm_smem_init();
 	msm_init_modem_notifier_list();
 	msm_smd_init();
 	msm_rpm_driver_init();
@@ -136,6 +98,110 @@ void __init msm8974_add_drivers(void)
 		msm_clock_init(&msm8974_clock_init_data);
 	tsens_tm_init_driver();
 	msm_thermal_device_init();
+}
+
+#define DISP_ESD_GPIO 28
+#define DISP_LCD_UNK_GPIO 62
+static void __init oppo_config_display(void)
+{
+	int rc;
+
+	rc = gpio_request(DISP_ESD_GPIO, "disp_esd");
+	if (rc) {
+		pr_err("%s: request DISP_ESD GPIO failed, rc: %d",
+				__func__, rc);
+		return;
+	}
+
+	rc = gpio_tlmm_config(GPIO_CFG(DISP_ESD_GPIO, 0,
+				GPIO_CFG_INPUT,
+				GPIO_CFG_PULL_DOWN,
+				GPIO_CFG_2MA),
+			GPIO_CFG_ENABLE);
+	if (rc) {
+		pr_err("%s: unable to configure DISP_ESD GPIO, rc: %d",
+				__func__, rc);
+		gpio_free(DISP_ESD_GPIO);
+		return;
+	}
+
+	rc = gpio_direction_input(DISP_ESD_GPIO);
+	if (rc) {
+		pr_err("%s: set direction for DISP_ESD GPIO failed, rc: %d",
+				__func__, rc);
+		gpio_free(DISP_ESD_GPIO);
+		return;
+	}
+
+	if (get_pcb_version() >= HW_VERSION__20) {
+		rc = gpio_request(DISP_LCD_UNK_GPIO, "lcd_unk");
+		if (rc) {
+			pr_err("%s: request DISP_UNK GPIO failed, rc: %d",
+					__func__, rc);
+			return;
+		}
+
+		rc = gpio_direction_output(DISP_LCD_UNK_GPIO, 0);
+		if (rc) {
+			pr_err("%s: set direction for DISP_LCD_UNK GPIO failed, rc: %d",
+					__func__, rc);
+			gpio_free(DISP_LCD_UNK_GPIO);
+			return;
+		}
+	}
+}
+
+static void __init oppo_config_ramconsole(void)
+{
+	int ret;
+
+	ram_console_dev = platform_device_alloc("ram_console", -1);
+	if (!ram_console_dev) {
+		pr_err("%s: Unable to allocate memory for RAM console device",
+				__func__);
+		return;
+	}
+
+	ret = platform_device_add(ram_console_dev);
+	if (ret) {
+		pr_err("%s: Unable to add RAM console device", __func__);
+		return;
+	}
+}
+
+static struct rpm_regulator* sns_reg = 0;
+static struct delayed_work sns_dwork;
+
+static void oppo_config_sns_reg_release(struct work_struct *work)
+{
+	int ret;
+	pr_info("Releasing sensor regulator\n");
+	if (sns_reg) {
+		ret = rpm_regulator_disable(sns_reg);
+		if (ret)
+			pr_err("8941_l18 rpm_regulator_disable failed (%d)\n", ret);
+		rpm_regulator_put(sns_reg);
+		sns_reg = 0;
+	}
+}
+
+static void __init oppo_config_sns_power(void)
+{
+	int ret;
+
+	sns_reg = rpm_regulator_get(NULL, "8941_l18");
+	if (IS_ERR_OR_NULL(sns_reg)) {
+		ret = PTR_ERR(sns_reg);
+		pr_err("8941_l18 rpm_regulator_get failed (%d)\n", ret);
+		sns_reg = 0;
+	} else {
+		ret = rpm_regulator_enable(sns_reg);
+		if (ret)
+			pr_err("8941_l18 rpm_regulator_enable failed (%d)\n", ret);
+
+		INIT_DELAYED_WORK(&sns_dwork, oppo_config_sns_reg_release);
+		schedule_delayed_work(&sns_dwork, msecs_to_jiffies(30000));
+	}
 }
 
 static struct of_dev_auxdata msm_hsic_host_adata[] = {
@@ -154,14 +220,6 @@ static struct of_dev_auxdata msm8974_auxdata_lookup[] __initdata = {
 			"usb_bam", NULL),
 	OF_DEV_AUXDATA("qcom,spi-qup-v2", 0xF9924000, \
 			"spi_qsd.1", NULL),
-	OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF9824000, \
-			"msm_sdcc.1", NULL),
-	OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF98A4000, \
-			"msm_sdcc.2", NULL),
-	OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF9864000, \
-			"msm_sdcc.3", NULL),
-	OF_DEV_AUXDATA("qcom,msm-sdcc", 0xF98E4000, \
-			"msm_sdcc.4", NULL),
 	OF_DEV_AUXDATA("qcom,sdhci-msm", 0xF9824900, \
 			"msm_sdcc.1", NULL),
 	OF_DEV_AUXDATA("qcom,sdhci-msm", 0xF98A4900, \
@@ -191,75 +249,29 @@ static void __init msm8974_map_io(void)
 	msm_map_8974_io();
 }
 
-extern int __init board_mfg_mode_init(void);
-extern int __init start_reason_init(void);
-extern int __init boot_mode_init(void);
-
-static void oppo_configure_disp_esd(void)
-{
-	int rc;
-
-	rc = gpio_request(28, "disp_esd");
-	if (rc) {
-		pr_err("%s: request ESD gpio failed, rc=%d\n", __func__, rc);
-		gpio_free(28);
-		return;
-	}
-	rc = gpio_tlmm_config(GPIO_CFG(
-			28, 0,
-			GPIO_CFG_INPUT,
-			GPIO_CFG_PULL_DOWN,
-			GPIO_CFG_2MA),
-			GPIO_CFG_ENABLE);
-	if (rc) {
-		pr_err("%s: unable to ESD config tlmm = 28\n", __func__);
-		gpio_free(28);
-		return;
-	}
-
-	rc = gpio_direction_input(28);
-	if (rc) {
-		pr_err("%s: set_direction for ESD GPIO failed, rc=%d\n", __func__, rc);
-		gpio_free(28);
-	}
-}
-
 void __init msm8974_init(void)
 {
 	struct of_dev_auxdata *adata = msm8974_auxdata_lookup;
 
+	/*
+	 * populate devices from DT first so smem probe will get called as part
+	 * of msm_smem_init.  socinfo_init needs smem support so call
+	 * msm_smem_init before it.  msm_8974_init_gpiomux needs socinfo so
+	 * call socinfo_init before it.
+	 */
+	board_dt_populate(adata);
+
+	msm_smem_init();
+
 	if (socinfo_init() < 0)
 		pr_err("%s: socinfo_init() failed\n", __func__);
 
-	board_mfg_mode_init();
-	start_reason_init();
-	boot_mode_init();
-
 	msm_8974_init_gpiomux();
 	regulator_has_full_constraints();
-	board_dt_populate(adata);
 	msm8974_add_drivers();
-	oppo_configure_disp_esd();
-}
-
-static struct persistent_ram_descriptor msm_prd[] __initdata = {
-	{
-		.name = "ram_console",
-		.size = SZ_1M,
-	},
-};
-
-static struct persistent_ram msm_pr __initdata = {
-	.descs = msm_prd,
-	.num_descs = ARRAY_SIZE(msm_prd),
-	.start = PLAT_PHYS_OFFSET + SZ_1G + SZ_256M,
-	.size = SZ_1M,
-};
-
-void __init msm8974_init_very_early(void)
-{
-	msm8974_early_memory();
-	persistent_ram_early_init(&msm_pr);
+	oppo_config_display();
+	oppo_config_ramconsole();
+	oppo_config_sns_power();
 }
 
 static const char *msm8974_dt_match[] __initconst = {
@@ -276,7 +288,6 @@ DT_MACHINE_START(MSM8974_DT, "Qualcomm MSM 8974 (Flattened Device Tree)")
 	.timer = &msm_dt_timer,
 	.dt_compat = msm8974_dt_match,
 	.reserve = msm_8974_reserve,
-	.init_very_early = msm8974_init_very_early,
 	.restart = msm_restart,
 	.smp = &msm8974_smp_ops,
 MACHINE_END
